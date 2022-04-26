0c8fa7f3e52dd81cce24f8d563a9e661794902da
Yue Guo <pangli@Yues-MacBook-Pro.local>
2022-04-25 22:36:06 -0500

    comment on sql code file

--- load_vfb_data.sql
+++ load_vfb_data.sql
@@ -1,8 +1,10 @@
--- create vfb database
+-- Create vfb database
 DROP DATABASE IF EXISTS vfb;
 CREATE DATABASE IF NOT EXISTS vfb;
 USE vfb;
 
+-- Create vfb mega table: all_plays. There are 30 variables in total
+-- This is the table with all the variables of the dataset we originally have
 DROP TABLE IF EXISTS all_plays;
 CREATE TABLE IF NOT EXISTS all_plays (
     I_D BIGINT,
@@ -37,6 +39,11 @@ CREATE TABLE IF NOT EXISTS all_plays (
     year VARCHAR(255)
 );
 
+-- Load data
+-- Some values in the dataset is '' value and its datatype in the magetable is INt, which will casue problem when we load data.
+-- Therefore, we will change these '' value into NULL value. 
+-- Also ignore the first line of variable names
+
 LOAD DATA LOW_PRIORITY
 INFILE "/Users/pangli/Desktop/all_plays2 (1) copy.csv"
 INTO TABLE vfb.all_plays
@@ -55,6 +62,13 @@ INTO TABLE vfb.all_plays
         
         
 -- Decompose tables
+-- There are 8 tables after the decompasation 
+-- There are also created two additional log tables to record additions, deletions, and changes
+
+
+-- Create Team offense table
+-- This table record which teams have been offense and the corresponding conference and year. 
+-- The datatypes in this table are all VARCHAR(255)
 DROP TABLE IF EXISTS team_offense;
 CREATE TABLE team_offense(
 offense                  VARCHAR(255), 
@@ -64,6 +78,9 @@ PRIMARY KEY(offense,year)
 );
 
 
+-- Create Team defense table
+-- This table record which teams have been defense and the corresponding conference and year. 
+-- The datatypes in this table are all VARCHAR(255)
 DROP TABLE IF EXISTS team_defense;
 CREATE TABLE team_defense(
 defense                   VARCHAR(255),
@@ -72,6 +89,10 @@ defense_conference        VARCHAR(255),
 PRIMARY KEY(defense,year)
 );
 
+
+-- Create Game table
+-- This table record all the game information such as home, away, season and year
+-- In this  table, week, season should be positive number. 
 DROP TABLE IF EXISTS game;
 CREATE TABLE game(
  game_id BIGINT,
@@ -82,6 +103,10 @@ CREATE TABLE game(
  year    VARCHAR(255),
  PRIMARY KEY (game_id)
 );
+
+-- Create Drive table
+-- This table record the drive information include which game id this drive belongs to.
+-- The game_id in this table is a foreign key referring to Game table, including sync updating and deleting
 
 DROP TABLE IF EXISTS drive;
 CREATE TABLE drive(
@@ -95,6 +120,9 @@ CONSTRAINT fk_game_id FOREIGN KEY (game_id)
     ON UPDATE CASCADE
 );
 
+-- Create Playtype table
+-- This table records all diiferent playtypes in football game and whether the playtype can cause scoring.
+
 DROP TABLE IF EXISTS playtype;
 CREATE TABLE playtype(
 play_type         VARCHAR(255), 
@@ -102,7 +130,9 @@ scoring           VARCHAR(255),
 PRIMARY KEY (play_type )
 );
 
-
+-- Create Play table
+-- On drive can have different plays. This table includes part of the information like which team is deffense and which team is offense in one play.alter
+-- defense and offense are foreign keys in this table referring to team defense table and team offense table.
 DROP TABLE IF EXISTS play;
 CREATE TABLE play(
  drive_id               BIGINT,
@@ -128,7 +158,9 @@ CONSTRAINT fk_defense FOREIGN KEY (defense)
 );
 
 
-
+-- Create play_drive table
+-- This part records the smaller-grained dimension information in the play.
+-- Play type, drive id and play number are the forein keys in this table. 
 DROP TABLE IF EXISTS play_drive;
 CREATE TABLE play_drive(
  I_D          BIGINT               AUTO_INCREMENT,
@@ -154,6 +186,8 @@ CREATE TABLE play_drive(
     ON UPDATE CASCADE
 );
 
+-- Create drive dairy table. 
+-- This table records the operations happen on the tables which have drive information.
 DROP TABLE IF EXISTS drive_dairy;
 CREATE TABLE drive_dairy(
 action_id        INT               AUTO_INCREMENT,
@@ -164,6 +198,8 @@ action           VARCHAR(255)      CHECK(action IN ("insert", "update","delete")
 PRIMARY KEY(action_id)
 );
 
+-- Create game dairy table
+-- This table records the operation happen on the Game table.
 DROP TABLE IF EXISTS game_dairy;
 CREATE TABLE game_dairy(
 action_id        INT               AUTO_INCREMENT,
@@ -180,7 +216,7 @@ PRIMARY KEY(action_id)
 
 
 
--- INSERT DATA
+-- Insert data from maga table into each decompsition table
 INSERT INTO game(game_id, home,away, week,season,year)
 SELECT DISTINCT game_id, home,away, week,season,year
 FROM all_plays;
@@ -212,8 +248,7 @@ SELECT DISTINCT I_D,drive_id,play_number,clock,yard_line,yards_to_goal,down,dist
 FROM all_plays;
 
 
--- Check the final count
-
+-- Check whether the data number in the decomposition table is the same with the data number in the maga table
 SELECT COUNT(*)
 FROM play_drive
 	JOIN play USING(drive_id ,play_number)
@@ -225,12 +260,19 @@ FROM play_drive
     
 SELECT COUNT(*)
 FROM all_plays;
+-- From the result we can see that the numbers are the same. 
+
 
 -- Features
+-- Following codes include all the functions we accomplished in the front end and data base(after decomposition
+-- We have 5 triggers, 7 procedures(include 1 transaction) and 2 views.
 USE vfb;
 
 -- Triggers
 
+-- Create insert_game_check trigger
+-- This trigger will record the data after any inserting on the game table
+-- It records what values have been inserted in game table, the operation time and set action as "insert"
 DROP TRIGGER IF EXISTS insert_game_check
 DELIMITER //
 CREATE TRIGGER insert_game_check
@@ -245,6 +287,9 @@ VALUES (DEFAULT, NEW.game_id, NEW.home,NEW.away,NEW.week, NEW.season,NEW.year, S
 END//
 DELIMITER ;
 
+-- Create trigger update_game_check
+-- This trigger will record the data after any updating on the game table
+-- It records the old data before update, the operation time and set action as "update"
 DROP TRIGGER IF EXISTS update_game_check
 DELIMITER //
 CREATE TRIGGER update_game_check
@@ -259,7 +304,9 @@ VALUES (DEFAULT, OLD.game_id, OLD.home,OLD.away,OLD.week, OLD.season,OLD.year, S
 END//
 DELIMITER ;
 
-
+-- Create trigger delete_game_check
+-- This trigger will record the data after any deleting on the game table
+-- It records the old data before deleting, the operation time and set action as "delete"
 DROP TRIGGER IF EXISTS delete_game_check
 DELIMITER //
 CREATE TRIGGER delete_game_check
@@ -275,7 +322,10 @@ END//
 DELIMITER ;
 
 
-
+-- Create trigger insert_drive_check1
+-- If distance > yards_gained (inserted), then down should plus 1. Otherewise down should be 1
+-- If down is larger than 4, the we need rto swao the defense and offense. In the meanwhile set down as
+-- Generate drive id based on play number and game id
 DROP TRIGGER IF EXISTS insert_drive_check1;
 DELIMITER //
 CREATE TRIGGER insert_drive_check1
@@ -305,11 +355,11 @@ IF NEW.down > 4 THEN
 
 END IF;
 END //
-
-
 DELIMITER ;
 
-
+-- Create trigger insert_drive_check2
+-- This trigger will record the data after any inserting tables with drive id
+-- It records the new data after inserting, the operation time and set action as "delete"
 DROP TRIGGER IF EXISTS insert_drive_check2;
 DELIMITER //
 CREATE TRIGGER insert_drive_check2
@@ -327,14 +377,19 @@ DELIMITER ;
 
 
 -- Procedures
--- insert_game
+
+-- Create insert_game
+-- This procedure update the values which has been pass from the front end.
+-- If insert failed because of duplicate key, it will return the error message. 
+-- Otherwise, it will return “insert successfully” message
+
 USE vfb;
 DROP PROCEDURE IF EXISTS insert_game;
 DELIMITER //
 CREATE PROCEDURE insert_game(IN gameid BIGINT, IN home VARCHAR(255),IN away VARCHAR(255), IN week TINYINT, IN season INT, IN year VARCHAR(255))
 BEGIN
 
-DECLARE EXIT HANDLER FOR 1062 SELECT "Insert Failed because of duplicate key";
+DECLARE EXIT HANDLER FOR 1062 SELECT "Insert Failed because the game id has already exists";
 
 INSERT INTO game(game_id, home,away, week,season,year)
 VALUES (gameid, home,away,week,season,year);
@@ -345,15 +400,10 @@ END //
 DELIMITER ;
 
 
-
-
-
-
-
-
-
-
--- delete_game
+-- Create procedure delete_game
+-- This procedure delete the data record realted with the game id in game table passed form the front end
+-- It checks whether the game id exists in game table. If not, it will return a error message. 
+-- If does exist, it operates deleting and return a success message.
 USE vfb;
 DROP PROCEDURE IF EXISTS delete_game;
 DELIMITER //
@@ -361,7 +411,7 @@ CREATE PROCEDURE delete_game (IN gameid BIGINT)
 BEGIN 
 
 IF NOT EXISTS(SELECT * from vfb.game where game_id = gameid) THEN
-	SELECT CONCAT("Game ", gameid, " Doesn't exist");
+	SELECT CONCAT("Game ", gameid, " doesn't exist");
 ELSE
 	DELETE FROM game 
 	WHERE game_id = gameid;
@@ -370,7 +420,9 @@ END IF;
 END //
 DELIMITER ;
 
--- update_game
+-- Create procedure update_game
+-- It checks whether the game id put into the procedure exist in Game table. If not, it will return an error message
+-- If exists, it will operate updating and return a successful message
 USE vfb;
 DROP PROCEDURE IF EXISTS update_game;
 DELIMITER //
@@ -378,15 +430,19 @@ CREATE PROCEDURE update_game(IN a BIGINT, IN b VARCHAR(255),IN c VARCHAR(255), I
 BEGIN
 
 IF NOT EXISTS (select * from vfb.game where game_id = a) THEN
-    SELECT CONCAT("GAME ID ",a, " doesn't exist");
+    SELECT CONCAT("Game ID ",a, " doesn't exist");
 ELSE
     UPDATE game SET home=b, away=c, week=d, season=e, year=f WHERE game_id=a;
-    SELECT CONCAT("GAME ID ",a," updated sucessfully");
+    SELECT CONCAT("Game ID ",a," updated sucessfully");
 END IF;
 END //
 DELIMITER ;
 
--- insert_drive
+-- Create procedure insert_drive
+-- It checks whether the game id put into the procedure exist in Game table. If not, it will return an error message 
+-- Then it checks whether down value satisfy the football rules. If not, it will return an error message
+-- Finally all the conditions has been satisfied, it use transaction to insert all the values into three tables. If insert successfully, there will be a success message
+-- Otherewise, it returns an error message telling users transaction has been rolled back
 USE vfb;
 DROP PROCEDURE IF EXISTS insert_drive;
 DELIMITER //
@@ -428,23 +484,11 @@ END //
 DELIMITER ;
 
 
-DROP PROCEDURE IF EXISTS exposive_play;
-DELIMITER //
-CREATE PROCEDURE exposive_play()
-BEGIN
-SELECT SUM(CASE WHEN (play_type LIKE "%Pass%" AND yards_gained > 16) THEN 1 ELSE 0 END) AS explosive_play_number,
-       SUM(offense_score) AS off_tds
-FROM play_drive
-	JOIN play USING(drive_id,play_number)
-    JOIN drive USING(drive_id)
-GROUP BY game_id;
-END //
-DELIMITER ;
-
-
--- Dashboard procedure
--- updated explosive_play
--- verified it works correctly at game level
+-- Create procedure explosive_play()
+-- Use the team value and season value to calculate the explosive play numbers and offense touchdowns of each team in different seasons
+-- explosive number is the play number which offense team yards gained larger than 16 team when play type is pass
+-- Or offense team yards gained larger than 12 when play type is rush
+-- offensive_touchdowns is the number of times when play type is Passing Touchdown or Rushing Touchdown.
 DROP PROCEDURE IF EXISTS explosive_play;
 DELIMITER //
 CREATE PROCEDURE explosive_play(IN team_value VARCHAR(255),IN season_value INT)
@@ -459,11 +503,13 @@ SELECT  SUM(CASE WHEN ((play_type LIKE "%Pass%" AND yards_gained > 16) OR
              JOIN  drive USING (drive_id)
              JOIN (SELECT * FROM game WHERE season = season_value) b USING (game_id)
 	GROUP BY game_id;
-
 END //
-
 DELIMITER ;
 
+-- Create procedure dashboard
+-- This procedure use team value and season value to calculate the parameters we want from the original data
+-- pass_attempt is the sum of pass_completion1, pass_td, pass_interception and pass_incomplete in each game
+-- pass_completion (new) is the sum of pass_completion1, pass_td in each game
 DROP PROCEDURE IF EXISTS dashboard;
 DELIMITER //
 CREATE PROCEDURE dashboard(IN team_value VARCHAR(255),IN season_value INT)
@@ -493,16 +539,13 @@ END //
 
 DELIMITER ;
 
-
-CALL dashboard('Arkansas', 2018);
-
 -- Views
 USE vfb;
 
--- create a view of the game_id, season, home, away, home_score, away_score
--- from the play drive and game tables
--- home_score should be the max offense_score for the home team in the game when offense is home
--- away_score should be the max offense_score for the away team in the game when offense is away
+-- Create a view of the game_id, season, home, away, home_score, away_score
+-- From the play drive and game tables
+-- Home_score should be the max offense_score for the home team in the game when offense is home
+-- Away_score should be the max offense_score for the away team in the game when offense is away
 DROP VIEW IF EXISTS game_score;
 CREATE VIEW game_score AS
 SELECT game.game_id, game.season, game.home, game.away,
@@ -514,17 +557,13 @@ JOIN game ON drive.game_id = game.game_id
 GROUP BY game.game_id, game.season, game.home, game.away;
 
 
--- create team score view
-USE vfb;
-
+-- Create team score view
+-- This view shows how many games a team wins in different season
+-- Find the winner of each game and calculate the number group by winner
 DROP VIEW IF EXISTS team_score;
 CREATE VIEW team_score AS
 SELECT season,(CASE WHEN home_score > away_score THEN home ELSE away END) AS winner,count(*) as wins
 FROM game_score
 GROUP BY winner,season 
 HAVING season IS NOT NULL;
-
-
-
-
 
